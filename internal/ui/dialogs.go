8c0373bad82dd0f0c054c60bb2440191b0c7bb54
Larkin <88820972+larkin1@users.noreply.github.com>
2026-01-12 17:38:57 +1100

    Add dialog utilities

---
+++ internal/ui/dialogs.go
@@ -0,0 +1,24 @@
+package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+// Placeholder for dialog utilities
+// Can be expanded for item search dialogs, etc.
+
+type ItemSearchDialog struct {
+	onSelect func(name string)
+}
+
+func NewItemSearchDialog(onSelect func(name string)) *ItemSearchDialog {
+	return &ItemSearchDialog{
+		onSelect: onSelect,
+	}
+}
+
+func (d *ItemSearchDialog) Show(parent fyne.Window) {
+	// Placeholder
+	// Would implement fuzzy search and selection
+}
